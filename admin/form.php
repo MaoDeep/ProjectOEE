ed7c0170eb4c190bdf467a47c62da3ae59421de9
sudarat54 <97605991+sudarat54@users.noreply.github.com>
2022-08-16 16:57:51 +0700

    Update form.php

--- admin/form.php
+++ admin/form.php
@@ -104,7 +104,7 @@ if (empty($_SESSION["status"]) || $_SESSION["status"] !== "Admin") {
             <li class="nav-item">
                 <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
                     <i class="fas fa-fw fa-clipboard"></i>
-                    <span>จัดการข้อมูล</span>
+                    <span>จัดการข้อมู</span>
                 </a>
                 <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
