33ad28d57124e9a5c7842dd3b671c4b833638dc7
luroose <636051200195@mail.rmutk.ac.th>
2022-09-13 17:38:22 +0700

    Update del.php

--- admin/post/del.php
+++ admin/post/del.php
@@ -1,14 +1,22 @@
 <?php
 include "../config.php";
 if (isset($_GET["id"]) && $_GET["id"] !== "") {
-      header('Location: ../user.php');
-      $sql = "DELETE FROM users WHERE users.u_id = " . $_GET["id"];
-      $re = mysqli_query($conn, $sql);
-      if ($re) {
-            echo '<script>alert("บันทึกสำเร็จ")</script>';
-      } else {
-            echo '<script>alert("บันทึกไม่สำเร็จ")</script>';
-      }
-       
+  header('Location: ../user.php');
+  $sql = "DELETE FROM users WHERE users.u_id = " . $_GET["id"];
+  $re = mysqli_query($conn, $sql);
+  if ($re) {
+    echo '<script>alert("บันทึกสำเร็จ")</script>';
+  } else {
+    echo '<script>alert("บันทึกไม่สำเร็จ")</script>';
+  }
 }
 ?>
+<script>
+  function chkdel() {
+    if (confirm('  กรุณายืนยันการลบอีกครั้ง !!!  ')) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+</script>
